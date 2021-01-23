e73af36e31b71143e685fa0394d09af133ef7551
Joe Iannone <joseph.m.iannone@gmail.com>
2021-01-23 18:09:43 -0500

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers'
 
 const store = configureStore({reducer: reducer});
+
 store.subscribe(() => {
   console.log(store.getState())
 })
