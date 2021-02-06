2ec91e4511c7d970a624967a85d33d78fbc534d9
Joe Iannone <joseph.m.iannone@gmail.com>
2021-02-06 11:10:24 -0500

    profile and settings drawers

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Peer, { DataConnection } from 'peerjs';
 
-import { Button, AppBar, Toolbar, IconButton, Typography, MenuItem, Menu } from '@material-ui/core';
+import { Button, AppBar, Toolbar, IconButton, Typography, MenuItem, Menu, Drawer } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import SettingsIcon from '@material-ui/icons/Settings';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -33,9 +34,10 @@ type PeerBarProps = {
 
 const AppHeader: React.FC<PeerBarProps> = (props: PeerBarProps) => {
 
-  const [anchorEl, setAnchorEl] = React.useState<any>(null);
-  const profileMenuRef = React.useRef();
-  const open = Boolean(anchorEl);
+  const [profileAnchorEl, setProfileAnchorEl] = useState<null | HTMLElement>(null);
+  const [settingsAnchorEl, setSettingsAnchorEl] = useState<null | HTMLElement>(null);
+  const profileDrawerOpen = Boolean(profileAnchorEl);
+  const settingsDrawerOpen = Boolean(settingsAnchorEl);
 
   const logout = () => {
     fetch('/logout', { method: 'POST', headers: {'Content-Type': 'application/json'}})
@@ -50,13 +52,25 @@ const AppHeader: React.FC<PeerBarProps> = (props: PeerBarProps) => {
     });
   }
 
-  const handleMenu = (event: React.MouseEvent) => {
-    setAnchorEl(profileMenuRef.current);
-    console.log(profileMenuRef.current);
+  const openProfileDrawer = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const openSettingsDrawer = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileAnchorEl(event.currentTarget);
+  };
+
+  const closeDrawers = (event: React.KeyboardEvent | React.MouseEvent) => {
+    
+    if (
+      event.type === 'keydown' && (
+        (event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift'
+      )
+    ) return;
+    
+    setProfileAnchorEl(null);
+    setSettingsAnchorEl(null);
   };
 
 
@@ -68,18 +82,33 @@ const AppHeader: React.FC<PeerBarProps> = (props: PeerBarProps) => {
         </IconButton>
         <Typography variant="h6">p2pChat</Typography>
         <Button color="inherit" onClick={logout} >Logout</Button>
-        <div>
+      
           <IconButton
-            aria-label="account of current user"
+            aria-label="profile form of current user"
             aria-controls="menu-appbar"
             aria-haspopup="true"
-            onClick={handleMenu}
+            onClick={openProfileDrawer}
             color="inherit"
           >
             <AccountCircle />
           </IconButton>
-        </div>
+          <IconButton
+            aria-label="settings form"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={openSettingsDrawer}
+            color="inherit"
+          >
+            <SettingsIcon />
+          </IconButton>
+        
       </Toolbar>
+      <Drawer anchor={'right'} open={profileDrawerOpen} onClose={closeDrawers}>
+        <div>profile form</div>
+      </Drawer>
+      <Drawer anchor={'right'} open={settingsDrawerOpen} onClose={closeDrawers}>
+        <div>setting form</div>
+      </Drawer>
     </AppBar>      
   );
 
